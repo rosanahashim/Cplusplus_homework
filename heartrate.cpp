0e8e1af0ad77e37dada49b80d2c6959703c1d3cc
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2022-02-27 23:15:00 -0500

    create set and get functions

--- heartrate.cpp
+++ heartrate.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+class HeartRates {
+private:
+    string fn;
+    string ln;
+    int day;
+    int month;
+    int year;
+public:
+    HeartRates(string fn, string ln, int day, int month, int year) : fn(fn), ln(ln), day(day), month(month), year(year) {}
+
+    void setFirstName(string inputFN) {
+        fn = inputFN;
+    }
+
+    void setLastname(string inputLN) {
+        ln = inputLN;
+    }
+
+    void setDay(int inputDay) {
+        day = inputDay;
+    }
+
+    void setYear(int inputYear) {
+        year = inputYear;
+    }
+
+    void setMonth(int inputMonth) {
+        month = inputMonth;
+    }
+
+    string getFirstName() {
+        return fn;
+    }
+
+    string getLastName() {
+        return ln;
+    }
+
+};
+
+
 
 int main() {
    cout << "Hello, world" << endl;
